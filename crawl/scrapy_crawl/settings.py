43480800438b3eb8978da762b00a301bc936a7a3
Anton Shyshko <pozernishku@gmail.com>
2023-01-09 23:45:15 +0200

    feat: update settings -> ROBOTSTXT_OBEY True -> False

--- crawl/scrapy_crawl/settings.py
+++ crawl/scrapy_crawl/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "scrapy_crawl.spiders"
 # USER_AGENT = 'scrapy_crawl (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
