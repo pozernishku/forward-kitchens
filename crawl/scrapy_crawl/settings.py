176207ec1387b926fff128ff5ad845fe398b56b8
Anton Shyshko <pozernishku@gmail.com>
2023-01-11 00:37:57 +0200

    feat: update settings -> activate proxy middleware

--- crawl/scrapy_crawl/settings.py
+++ crawl/scrapy_crawl/settings.py
@@ -48,11 +48,17 @@ TELNETCONSOLE_ENABLED = False
 #    'scrapy_crawl.middlewares.ScrapyCrawlSpiderMiddleware': 543,
 # }
 
+SMARTPROXY_COUNTRY = "random"
+PROXY_DETAILS = {
+    "random": ("gate.smartproxy.com", "7000"),
+    "us": ("us.smartproxy.com", "10000"),
+}
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'scrapy_crawl.middlewares.ScrapyCrawlDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    # "scrapy_crawl.middlewares.ScrapyCrawlDownloaderMiddleware": 543,
+    "scrapy_crawl.middlewares.ProxyMiddleware": 740,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
