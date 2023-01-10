f916c43bc4dc617c856f02ab23d0e8fd1b8d6961
Anton Shyshko <pozernishku@gmail.com>
2023-01-11 00:40:22 +0200

    feat: update grubhub_spider -> add custom_settings (SMARTPROXY_COUNTRY)

--- crawl/scrapy_crawl/spiders/grubhub_spider.py
+++ crawl/scrapy_crawl/spiders/grubhub_spider.py
@@ -4,6 +4,7 @@ import scrapy
 class GrubhubSpiderSpider(scrapy.Spider):
     name = "grubhub_spider"
     allowed_domains = ["www.grubhub.com", "grubhub.com"]
+    custom_settings = {"SMARTPROXY_COUNTRY": "us"}
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         pass
