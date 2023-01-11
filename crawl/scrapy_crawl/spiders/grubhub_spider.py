f118e9944836093a27b0f6af09c5afad2e742980
Anton Shyshko <pozernishku@gmail.com>
2023-01-11 23:25:30 +0200

    feat: update grubhub_spider module -> add start_scrapy_crawl()

--- crawl/scrapy_crawl/spiders/grubhub_spider.py
+++ crawl/scrapy_crawl/spiders/grubhub_spider.py
@@ -1,5 +1,8 @@
 import scrapy
 
+import project_settings as s
+from crawl.starter import start_scrapy_crawl
+
 
 class GrubhubSpiderSpider(scrapy.Spider):
     name = "grubhub_spider"
@@ -8,3 +11,13 @@ class GrubhubSpiderSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         pass
+
+
+if __name__ == "__main__":
+    spider_names = ["grubhub_spider"]
+
+    start_scrapy_crawl(
+        spider_names=spider_names,
+        max_requests=1,
+        output_folder=s.OUTPUT_FOLDER,
+    )
