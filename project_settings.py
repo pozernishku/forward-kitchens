e7ff123d23d70972b5a9ca9d76350004dac99367
Anton Shyshko <pozernishku@gmail.com>
2023-01-11 23:10:46 +0200

    feat: introduce project_settings.py

---
+++ project_settings.py
@@ -0,0 +1,15 @@
+import os
+
+SOURCE_TO_MARKET_MAPPING = {
+    "grubhub_spider": "us",
+}
+
+# Full path to project directory
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+OUTPUT_FOLDER = "crawl_output"
+
+FILE_NAME_FULL_LOG = "logs/all_logging_outputs.log"
+FILE_NAME_COMPACT_LOG = "logs/compact_logging_outputs.log"
+
+MAX_REQUESTS = 50000
