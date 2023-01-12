d30cd6456d1939e732839aae6dcd7f32042052f0
Anton Shyshko <pozernishku@gmail.com>
2023-01-12 23:08:50 +0200

    feat: introduce start_requests() in grubhub_spider module

--- crawl/scrapy_crawl/spiders/grubhub_spider.py
+++ crawl/scrapy_crawl/spiders/grubhub_spider.py
@@ -1,3 +1,6 @@
+import json
+from typing import Iterator
+
 import scrapy
 
 import project_settings as s
@@ -13,6 +16,28 @@ class GrubhubSpiderSpider(scrapy.Spider):
     name = "grubhub_spider"
     allowed_domains = ["www.grubhub.com", "grubhub.com"]
     custom_settings = {"SMARTPROXY_COUNTRY": "us"}
+
+    def start_requests(self) -> Iterator[scrapy.Request]:
+        yield scrapy.Request(
+            url="https://api-gtm.grubhub.com/auth",
+            method="POST",
+            # callback=self.parse,
+            # errback=self.handle_errback,
+            headers={
+                "Content-Type": "application/json;charset=UTF-8",
+                "Authorization": "Bearer",
+            },
+            # cb_kwargs=dict(params=params),
+            body=json.dumps(
+                {
+                    "brand": BRAND,
+                    "client_id": CLIENT_ID,
+                    "device_id": DEVICE_ID,
+                    "scope": SCOPE,
+                }
+            ),
+            # dont_filter=True,
+        )
 
     def parse(self, response, **kwargs):
         pass
