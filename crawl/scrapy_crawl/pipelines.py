f7dae3a15317260b9dfbd1e23a7166419fee9926
Anton Shyshko <pozernishku@gmail.com>
2023-01-22 01:49:34 +0200

    feat: update ScrapyCrawlPipeline -> strip string fields

--- crawl/scrapy_crawl/pipelines.py
+++ crawl/scrapy_crawl/pipelines.py
@@ -11,8 +11,17 @@ from itemadapter import ItemAdapter
 class ScrapyCrawlPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
+        if isinstance(adapter.get("Category Name"), str):
+            adapter["Category Name"] = adapter["Category Name"].strip()
+        if isinstance(adapter.get("Item Name"), str):
+            adapter["Item Name"] = adapter["Item Name"].strip()
+        if isinstance(adapter.get("Item Description"), str):
+            # TODO: Get rid of multi-lines
+            adapter["Item Description"] = adapter["Item Description"].strip()
         if isinstance(adapter.get("Item Price"), int):
             adapter["Item Price"] = adapter["Item Price"] / 100
+        elif isinstance(adapter.get("Item Price"), str):
+            adapter["Item Price"] = adapter["Item Price"].strip()
         if isinstance(adapter.get(""), int):
             adapter[""] = adapter[""] / 100  # Option Price
         return item
